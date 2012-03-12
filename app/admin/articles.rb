4bee39e35b4da76a7824de955080f076b25535f4
«niksan» <info@niksan.ru>
2012-03-12 10:32:39 +0400

    some lite changes

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,15 +1,16 @@
 #encoding: utf-8
 ActiveAdmin.register Article do
 
-menu :label => "Статьи"
+  menu :label => "Статьи"
 
-index do
+  index do
     column :id
     column :title
     column :special_offer
     column :help_info
     column :parent
     column :created_at
-end
+    default_actions
+  end
   
 end
