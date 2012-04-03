c3fd29b451ea79b000069c25c3c41160e5c3efeb
«niksan» <info@niksan.ru>
2012-04-03 15:14:15 +0400

    add changes in design and update db dump

--- app/admin/partners.rb
+++ app/admin/partners.rb
@@ -1,3 +1,13 @@
+#encoding: utf-8
 ActiveAdmin.register Partner do
-  
+
+  form do |f|
+    f.inputs "details" do
+      f.input :name, label:'название'
+      f.input :partner_image
+      f.input :url
+    end
+    f.buttons
+  end
+
 end
