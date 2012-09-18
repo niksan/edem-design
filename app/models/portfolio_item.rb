977d09bd55c9d36a8d09f28b837802dfb4010593
Aleksandr <nikulinaleksandr@gmail.com>
2012-09-18 17:13:58 +0400

    change image path

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 class PortfolioItem < ActiveRecord::Base
   belongs_to :portfolio
-  image_url = "/system/:attachment/:id/:style/:basename.:extension"
-  has_attached_file :portfolio_item_image, :styles => { :medium => "800x600>", :thumb => "200x140#" }, url: image_url, path: image_url
+  path = ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
+  url = "/system/:attachment/:id/:style/:basename.:extension"
+  has_attached_file :portfolio_item_image, :styles => { :medium => "800x600>", :thumb => "200x140#" }, url: url, path: path
 end
