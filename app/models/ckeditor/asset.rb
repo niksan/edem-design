bfc34be6d96f2bda48c7632d31944c8daef4db9f
Aleksandr <nikulinaleksndr@gmail.com>
2012-11-19 11:16:14 +0400

    change ckeditor models

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,7 +1,4 @@
 class Ckeditor::Asset < ActiveRecord::Base
   include Ckeditor::Orm::ActiveRecord::AssetBase
-              
-  delegate :url, :current_path, :size, :content_type, :filename, :to => :data
-  
-  validates_presence_of :data
+  include Ckeditor::Backend::Paperclip
 end
