be7b4782216fbbab6b33caf817ded8c25da5d5ba
Aleksandr <nikulinaleksndr@gmail.com>
2012-11-20 09:39:47 +0400

    change deploy setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ load 'deploy/assets'
 
 ssh_options[:forward_agent] = true
 
+set :keep_releases, 3 #store releases
 set :application,     'edem-design'
 set :deploy_server,   "lithium.locum.ru"
 set :bundle_without,  [:development, :test]
