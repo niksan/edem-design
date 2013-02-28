9592e0c676a12788dfd1ad31eb45f06b20b060bf
NikSan <nikulinaleksandr@gmail.com>
2013-02-28 10:55:03 +0400

    update ruby -version

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.2.11'
+gem 'rails', '3.2.12'
 gem 'magic_encoding'
 
 gem 'mysql2'
 gem 'haml-rails'
 gem 'paperclip'
-gem 'ckeditor', "3.7.3"
+gem 'ckeditor'
 gem 'gritter_notices'
 gem 'permalink'
 gem 'awesome_nested_set'
@@ -14,21 +14,19 @@ gem 'russian'
 gem 'json'
 gem 'rails_admin'
 gem 'devise'
-gem 'yaml_db', git: 'git://github.com/lostapathy/yaml_db.git'
+gem 'yaml_db'
 gem 'execjs'
 gem 'therubyracer'
+gem 'bootstrap-sass'
+gem 'sass-rails'
+gem 'compass-rails'
+gem 'coffee-rails'
 
-#group :assets do
-  gem 'bootstrap-sass'
-  gem 'sass-rails'
-  gem 'compass-rails'
-  gem 'coffee-rails'
+group :assets do
   gem 'uglifier'
-#end
+end
 
 group :development do
-  gem 'rails-erd'
-  gem 'rnotify'
   gem 'letter_opener'
   gem 'thin'
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,10 +5,10 @@ ssh_options[:forward_agent] = true
 
 set :keep_releases, 3
 set :application,     'edem-design'
-set :deploy_server,   "lithium.locum.ru"
+set :deploy_server,   'lithium.locum.ru'
 set :bundle_without,  [:development, :test]
-set :user,            "hosting_niksan"
-set :login,           "niksan"
+set :user,            'hosting_niksan'
+set :login,           'niksan'
 set :use_sudo,        false
 set :deploy_to,       "/home/#{user}/projects/#{application}"
 set :unicorn_conf,    "/etc/unicorn/#{application}.#{login}.rb"
@@ -16,15 +16,15 @@ set :unicorn_pid,     "/var/run/unicorn/#{application}.#{login}.pid"
 set :bundle_dir,      File.join(fetch(:shared_path), 'gems')
 role :web,            deploy_server
 role :app,            deploy_server
-role :db,             deploy_server, :primary => true
-set :rvm_ruby_string, "1.9.3"
+role :db,             deploy_server, primary: true
+set :rvm_ruby_string, '2.0.0'
 set :rake,            "rvm use #{rvm_ruby_string} do bundle exec rake" 
 set :bundle_cmd,      "rvm use #{rvm_ruby_string} do bundle"
 set :scm,             :git
-set :repository,      "git://github.com/niksan/edem-design.git"
+set :repository,      'git://github.com/niksan/edem-design.git'
 
-before "deploy:assets:precompile", :remove_paths, :set_links
-after "deploy:update_code", :do_migrations
+before 'deploy:assets:precompile', :remove_paths, :set_links
+after 'deploy:update_code', :do_migrations
 
 task :remove_paths, roles => :app do
   run "rm -rf #{release_path}/tmp"
@@ -47,25 +47,25 @@ task :do_migrations, roles => :app do
 end
 
 before 'deploy:finalize_update', 'set_current_release'
-task :set_current_release, :roles => :app do
+task :set_current_release, roles: :app do
     set :current_release, latest_release
 end
 
   set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
 
 namespace :deploy do
-  desc "Start application"
-  task :start, :roles => :app do
+  desc 'Start application'
+  task :start, roles: :app do
     run unicorn_start_cmd
   end
 
-  desc "Stop application"
-  task :stop, :roles => :app do
+  desc 'Stop application'
+  task :stop, roles: :app do
     run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
   end
 
-  desc "Restart Application"
-  task :restart, :roles => :app do
+  desc 'Restart Application'
+  task :restart, roles: :app do
     run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
   end
 end
