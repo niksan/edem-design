406cdc75e36082472dcead18952cfecb92f1dbaf
NikSan <nikulinaleksandr@gmail.com>
2013-03-04 09:58:39 +0400

    change login page

--- app/models/special_offer.rb
+++ app/models/special_offer.rb
@@ -1,12 +1,13 @@
 class SpecialOffer < ActiveRecord::Base
 
   validate :name, presence: true
-
   belongs_to :article
   
   image_url = "/system/:attachment/:id/:style/:basename.:extension"
   image_path = ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
 
   has_attached_file :special_image, styles: { medium: "800x600>", thumb: "258x350>" }, url: image_url, path: image_path
+
+  default_scope order('public')
 
 end
