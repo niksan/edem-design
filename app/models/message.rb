c7ed2787f625494319ed1fbc3423acdb7901d57d
NikSan <nikulinaleksandr@gmail.com>
2013-11-28 17:22:25 +0400

    fix

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,6 @@ class Message
   include ActiveModel::Validations
   attr_accessor :name, :phone, :email, :body
   validates_presence_of :name, :phone, :email, :body
-  validates :email, presence: true, format: { with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
+  #validates :email, presence: true, format: { with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
 
 end
