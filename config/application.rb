168ffa7f0a7c673450dbf317aba24de739a96699
NikSan <nikulinaleksandr@gmail.com>
2013-11-28 19:16:56 +0400

    fix

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module EdemDesign
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    #config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
