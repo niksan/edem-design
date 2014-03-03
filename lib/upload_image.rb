a2412a24a9ec968234a07f4fa7da2469a0e5cdad
NikSan <nikulinaleksandr@gmail.com>
2014-03-03 10:06:44 +0400

    fix

--- lib/upload_image.rb
+++ lib/upload_image.rb
@@ -4,7 +4,7 @@ module UploadImage
 
     include CarrierWave::MiniMagick
 
-    storage :fog
+    storage :file
 
     def store_dir
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
