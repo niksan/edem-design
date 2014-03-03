33e4489189d063d96aee696dc64f93edfe8af95e
NikSan <nikulinaleksandr@gmail.com>
2014-03-03 17:10:10 +0400

    fix

--- lib/carrierwave_migrator.rb
+++ lib/carrierwave_migrator.rb
@@ -4,7 +4,6 @@ class CarrierwaveMigrator
     
     def portfolio
       require File.expand_path 'config/environment'
-      FileUtils.mkdir_p Rails.root.join('public', 'uploads')
 
       Portfolio.where('portfolio_image_file_name IS NOT NULL').each do |p|
         p.image = File.open(Rails.root.join('public', 'system', 'images', p.id.to_s, 'original', p.image_file_name))
