2a19ce07d34cafdf5b0825e881efdfcfb87c8fb6
NikSan <nikulinaleksandr@gmail.com>
2014-03-03 11:49:24 +0400

    fix

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,4 +1,5 @@
 class Ckeditor::Asset < ActiveRecord::Base
-  include Ckeditor::Orm::ActiveRecord::AssetBase
-  include Ckeditor::Backend::Paperclip
+    include Ckeditor::Orm::ActiveRecord::AssetBase
+    delegate :url, :current_path, :size, :content_type, :filename, :to => :data
+    validates_presence_of :data
 end
