c7f9fa8d56c105e52da16abd2a60e0ee9c3d883a
NikSan <nikulinaleksandr@gmail.com>
2014-06-24 23:24:05 +0400

    upd server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,15 +1,15 @@
 set :stage, :production
 
 #role :all, %w{82.196.2.187}
-role :app, %w{188.226.228.199}
-role :web, %w{188.226.228.199}
-role :db,  %w{188.226.228.199}
+role :app, %w{82.196.2.187}
+role :web, %w{82.196.2.187}
+role :db,  %w{82.196.2.187}
 
-server '188.226.228.199', user: 'root', roles: %w{all}#, my_property: :my_value
+server '82.196.2.187', user: 'root', roles: %w{app web db}#, my_property: :my_value
 
-server '188.226.228.199',
+server '82.196.2.187',
   user: 'root',
-  roles: %w{root},
+  roles: %w{app web db},
   ssh_options: {
     user: 'root', # overrides user setting above
     keys: %w(/home/niksan/.ssh/id_rsa),
