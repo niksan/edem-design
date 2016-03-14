2c418ba16a3649f49ac634c3348a23c778ce87d4
Aleksander Nikulin <nikulinaleksandr@gmail.com>
2016-03-14 07:22:39 +0300

    compile ckeditor assets

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ module EdemDesign
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += %w( ckeditor/* )
     config.time_zone = 'Moscow'
     config.i18n.default_locale = :ru
     config.encoding = "utf-8"
